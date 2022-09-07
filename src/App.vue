5ce913aebf57301652a8d3c1b7e51b322e686cba
Tengiz Gogisvanidze <tgogisvanidze@ip.local>
2022-09-07 16:47:07 +0400

    many changes

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,17 @@
 <template>
-  <TreeItem :node="datas.EN" />
+  <div class="search">
+    <input type="search" id="search" placeholder="Search Event...">
+    <img src="@/assets/search.png" alt="">
+  </div>
+  <div class="div">
+    <h2>All Games</h2>
+    <hr>
+    <TreeItem :node="datas.EN" />
+  </div>
+  <div class="div second">
+<h2>test teste tset</h2>
+  </div>
+
 </template>
 
 <script setup>
@@ -12,17 +24,71 @@ const sport = async () => {
   const resp = await axios.get('https://sportservice.inplaynet.tech/api/sport/getheader/en')
   datas.value = JSON.parse(resp.data)
   console.log((datas.value))
-  // const allData = ref({})
-  // for(const key in datas.value) {
-  //   const data = {
-  //     id : key,
-  //     name : datas.value.EN.Sports[key].Name,
-  //   }
-  //   allData.value.push(data)
-  // }
 }
 
 const datas = ref('')
 sport()
 
 </script>
+
+<style lang="scss">
+* {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+}
+
+body {
+  padding: 20px;
+}
+
+#search {
+  background-color: #2e3740;
+  color: #ffffff;
+  outline: none;
+  height: 60px;
+  width: 266px;
+  font-size: 15px;
+  margin-top: 46px;
+  padding-left: 20px;
+}
+
+img {
+  margin-left: -50px;
+  width: 20px;
+  height: 20px;
+}
+
+
+h2 {
+  color: #ffffff;
+  margin-bottom: 10px;
+}
+
+hr {
+  width: 265px;
+  border: 1px solid #fd402d;
+  display: inline-block;
+  margin-left: -20px;
+  margin-bottom: 10px;
+}
+
+.div {
+  padding: 20px;
+  height: 610px;
+  overflow-y: scroll;
+  overflow-x: hidden;
+  width: 266px;
+  background-color: #2e3740;
+  margin-top: 20px;
+
+  &::-webkit-scrollbar {
+    background: #2e3740;
+    width: 0;
+  }
+}
+
+.second {
+  margin-top: 20px;
+}
+</style>
--- src/components/TreeItem.vue
+++ src/components/TreeItem.vue
@@ -1,23 +1,23 @@
 <template>
     <div>
         <div @click="nodeClicked" class="node" :style="{ 'margin-left': `${depth * 20}px` }">
-            <span class="material=icons">{{ node?.Name }}</span>
+            <span class="material-icons">{{ node?.Name }}</span>
         </div>
         <div>
             <div>
-                <TreeItem class="sport-icon" :sport="child.KeyName.toLowerCase()" @click="nodeClicked"
+                <TreeItem class="sport-icon"  :class="expanded ? 'sport-icon:hover' : null" :sport="child.KeyName.toLowerCase()" @click="nodeClicked"
                     v-for="(child, index) in node?.Sports" :key="index" :node="child" :depth="depth + 1" />
             </div>
-            <div v-if="expanded">
+            <!-- <div v-if="expanded">
                 <TreeItem class="flag" :country="child.KeyName.toLowerCase()" v-for="(child, index) in node?.Regions"
                     :key="index" :node="child" :depth="depth + 1" />
             </div>
             <div v-if="expanded">
                 <TreeItem v-for="(child, index) in node?.Champs" :key="index" :node="child" :depth="depth + 6" />
-
-            </div>
+            </div> -->
         </div>
     </div>
+
 </template>
 
 <script setup>
@@ -47,36 +47,46 @@ const nodeClicked = () => {
 <style  lang="scss">
 body {
     background-color: #262930;
-
+    display: flex;
+    flex-direction: column;
 }
 
 .node {
     font-size: 18px;
     width: 100px;
-    
     display: flex;
-
+    flex-direction: column;
 
     &::before {
         content: '';
         color: #fff;
-
     }
 
     span {
-        margin-top: 10px;
-        background-color: #ccc;
+        color: #ffffff;
         cursor: pointer;
         width: 100px;
         text-align: center;
-        border-radius: 20px;
+        display: flex;
+        flex-direction: column;
     }
 
 }
 
 .sport-icon {
+    margin-top: 14px;
+    cursor: pointer;
     &::before {
-        color: #fff !important;
+        color: #ed3b2f !important;
+        display: flex;
+        flex-direction: column;
+    }
+    &:active:after {
+        background-color: #262930 ;
     }
 }
+// .hover {
+//     background-color: #262930 ;
+//     border: 2px solid #ed3b2f;
+// }
 </style>
